033fafaaf4245ce96347639738faf1526f252b6b
ChaoslordJames <jameswquinn@icloud.com>
2025-03-23 23:36:45 +0000

    Create GossipProtocolTests.swift

---
+++ Tests/GossipProtocolTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import EliasChaosFractalApple
+
+class GossipProtocolTests: XCTestCase {
+    func testStatePropagation() async {
+        print("Skipping network test in lite mode")
+    }
+}
